a32caaed8ba6b13082436cf4fda8f500190c1f02
n0nnnnc <napatchirawichada@gmail.com>
2024-08-21 14:21:56 +0700

    First commit

--- src/components/About.js
+++ src/components/About.js
@@ -17,7 +17,6 @@ function About() {
             unde dolor?
           </p>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
